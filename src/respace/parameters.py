d50098c259d51f361b511731621e594006065fdc
TLouf <loufthomas@gmail.com>
2023-02-07 12:50:11 +0100

    Add docstrings to parameters classes

--- src/respace/parameters.py
+++ src/respace/parameters.py
@@ -9,12 +9,25 @@ import pandas as pd
 
 @dataclass
 class Parameter:
+    """Represent a parameter with a mandatory default value.
+
+    Attributes
+    ----------
+    name : str
+        Name of the parameter.
+    default : Hashable
+        Default value for this parameter.
+    values : list[Hashable]
+        List of possible values for the parameter. Will contain the default in first
+        position even if it was not supplied thus at initialization.
+    """
+
     name: str
     default: Hashable
     values: list[Hashable] = field(default_factory=list)
 
     def __post_init__(self):
-        # put default in self.values[0]
+        # Put default in self.values[0].
         if len(self.values) == 0:
             self.values.append(self.default)
         elif self.values[0] != self.default:
@@ -22,11 +35,25 @@ class Parameter:
                 self.values.remove(self.default)
             self.values = [self.default] + self.values
 
-    def to_dict(self):
-        return {self.name: self.values}
-
 
 class ParameterSet:
+    """Hold a list of ``Parameter`` instances and facilitate iteration over them.
+
+    The ``Parameter`` instances will be sorted according to their `name` attribute.
+
+    Parameters
+    ----------
+    parameters : list[Parameter] | dict
+        Input list of `Parameter` instancess, or dictionary whose keys are parameter
+        names, and whose values are either a single value, which will be the
+        default, or a sequence of values, the first of which will be the default.
+
+    Attributes
+    ----------
+    parameters : list[Parameter]
+        Sorted list of `Parameter` instances.
+    """
+
     def __init__(self, parameters: list[Parameter] | dict):
         if isinstance(parameters, dict):
             parameters = [
@@ -41,5 +68,6 @@ class ParameterSet:
     def __iter__(self):
         return iter(self.parameters)
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
+        """Return a dictionary giving the possible values of all parameters."""
         return {p.name: p.values for p in self}
