3e76e1b682047b07b6194a235f8d72d1e7c768cd
TLouf <loufthomas@gmail.com>
2023-02-12 19:41:08 +0100

    Add `save_pickle` to `__all__`

--- src/respace/__init__.py
+++ src/respace/__init__.py
@@ -9,6 +9,7 @@ else:
 
 from respace.parameters import Parameter, ParameterSet
 from respace.result import ResultMetadata, ResultSet, ResultSetMetadata
+from respace.utils import save_pickle
 
 __all__ = [
     "Parameter",
@@ -16,6 +17,7 @@ __all__ = [
     "ResultMetadata",
     "ResultSet",
     "ResultSetMetadata",
+    "save_pickle",
 ]
 
 __version__ = metadata.version("respace")
