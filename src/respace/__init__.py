39a0764014e63c9146e7dfc5d9efb96d09cc701a
TLouf <loufthomas@gmail.com>
2023-03-19 14:37:46 +0100

    Remove useless usage of `importlib_metadata`

--- src/respace/__init__.py
+++ src/respace/__init__.py
@@ -1,11 +1,5 @@
 """ReSpace."""
-import sys
-
-if sys.version_info < (3, 10):
-    # compatibility for python <3.10
-    import importlib_metadata as metadata
-else:
-    from importlib import metadata
+from importlib import metadata
 
 from respace.parameters import Parameter, ParameterSet
 from respace.result import ResultMetadata, ResultSet, ResultSetMetadata
