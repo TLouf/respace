a8102cd058ee484846f8e2e156d16d400837d3c7
TLouf <loufthomas@gmail.com>
2023-03-25 19:56:48 +0100

    Add `test_set_compute_fun`

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -25,3 +25,17 @@ def test_save_path_fmt(simple_parameter, simple_result_set):
 
     simple_result_set.save_path_fmt = ""
     assert simple_result_set.save_path_fmt == ""
+
+
+def test_set_compute_fun(simple_result_metadata, simple_result_set):
+    res_name = simple_result_metadata.name
+    res_attrs = simple_result_set[res_name].attrs
+    og_tracking_compute_fun = res_attrs["tracking_compute_fun"]
+
+    def expected_compute_fun(p):
+        return p + 2
+
+    simple_result_set.set_compute_fun(res_name, expected_compute_fun)
+    assert res_attrs["compute_fun"] == expected_compute_fun
+    # For now we just check "tracking_compute_fun" has changed.
+    assert res_attrs["tracking_compute_fun"] != og_tracking_compute_fun
